8580b12c851741c7d05a61957147e645804060a9
kazawan <99900286+kazawan@users.noreply.github.com>
2023-05-30 11:58:15 +0800

    Create example.h

---
+++ 消抖/example.h
@@ -0,0 +1,17 @@
+#include "BTN.h"
+
+
+void setup() {
+  Serial.begin(115200);
+  pixels.begin();
+  btn_init(push_btn.pin);
+  
+}
+
+void loop() {
+  // ws_run(ws.lednum);
+  btn_scan(push_btn.pin,
+          &push_btn.keyup,
+          &push_btn.current_time,
+          push_btn.debouce_time);
+}
