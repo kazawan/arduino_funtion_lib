831a3d35fcf675460921bd89f35cfafb28686e7c
kazawan <55086403@qq.com>
2023-09-07 09:39:44 +0800

    new file:   "74HC165\346\214\211\351\224\256\346\211\253\346\217\217\346\266\210\346\212\226/hc165_lib/hc165.cpp" 	new file:   "74HC165\346\214\211\351\224\256\346\211\253\346\217\217\346\266\210\346\212\226/hc165_lib/hc165.h" 	new file:   "74HC165\346\214\211\351\224\256\346\211\253\346\217\217\346\266\210\346\212\226/hc165_lib/hc165.md"

---
+++ 74HC165按键扫描消抖/hc165_lib/hc165.cpp
@@ -0,0 +1,60 @@
+
+#include "hc165.h"
+
+void hc165_init(HC165_typeDef *hc165,BTN165_typeDef *btn165,int key_num)
+{
+    int i;
+    pinMode(hc165->data_pin, INPUT);
+    pinMode(hc165->clock_pin, OUTPUT);
+    pinMode(hc165->latch_pin, OUTPUT);
+    for(i=0;i<key_num;i++)
+    {
+        btn165[i].keyup = 1;
+        btn165[i].current_time = 0;
+        btn165[i].debouce_time = 30;
+        btn165[i].state = 0;
+    }
+};
+
+
+void hc165_scan(HC165_typeDef *hc165,KEYMAP_typeDef *key_map,BTN165_typeDef *btn165){
+    int j;
+    for(j = 0 ;j < sample_rate; j++)
+    {
+        LATCHDOWN(hc165->latch_pin);
+        LATCHUP(hc165->latch_pin);
+        if(j < sample_rate - 1)
+        {
+            for(int i = 0 ; i < hc165->key_num; i++)
+            {
+                btn165[i].state = digitalRead(hc165->data_pin);
+                CLKUP(hc165->clock_pin);
+                CLKDOWN(hc165->clock_pin);
+            }
+        }else if(j == sample_rate - 1)
+        {
+            for(int i = 0 ; i < hc165->key_num;i++)
+            {
+                if(btn165[i].state == 1 && btn165[i].keyup == 1 && btn165[i].state == digitalRead(hc165->data_pin))
+                {
+                    if(millis() - btn165[i].current_time > btn165[i].debouce_time)
+                    {
+                        btn165[i].keyup = 0;
+                        btn165[i].current_time = millis();
+                        // Serial.print("press: ");
+                        press_handler();
+                        Serial.println(key_map[i].key_value);
+                    }
+                }else if(btn165[i].keyup == 0 && btn165[i].state == 0)
+                {
+                    release_handler();
+                    Serial.println(key_map[i].key_value);
+                    btn165[i].keyup = 1;
+                }
+                CLKUP(hc165->clock_pin);
+                CLKDOWN(hc165->clock_pin);
+            }
+
+        }
+    }
+};
---
+++ 74HC165按键扫描消抖/hc165_lib/hc165.h
@@ -0,0 +1,40 @@
+#ifndef HC165_H
+#define HC165_H
+#include "Arduino.h"
+
+#define _weak_ __attribute__((weak))
+#define sample_rate 1000
+#define LATCHDOWN(latch) digitalWrite(latch,0)
+#define LATCHUP(latch) digitalWrite(latch,1)
+#define CLKDOWN(clock) digitalWrite(clock,0)
+#define CLKUP(clock) digitalWrite(clock,1)
+
+struct HC165_typeDef
+{
+    uint8_t data_pin;
+    uint8_t clock_pin;
+    uint8_t latch_pin;
+    int key_num;
+
+};
+
+struct KEYMAP_typeDef
+{
+    uint8_t key_value;
+};
+
+struct BTN165_typeDef
+{
+    int keyup;
+    int current_time;
+    unsigned int debouce_time; // 消抖时间
+    int state;
+};
+
+_weak_ void  press_handler();
+_weak_ void  release_handler();
+
+void hc165_init(HC165_typeDef *hc165,BTN165_typeDef *btn165,int key_num);
+void hc165_scan(HC165_typeDef *hc165,KEYMAP_typeDef *key_map,BTN165_typeDef *btn165);
+
+#endif
